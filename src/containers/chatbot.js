05fb26c03049c605b5009ab3d6db61bf5842a56d
Zafar Ahmed <zafar.ahmed2k17@gmail.com>
2023-12-25 15:53:46 +0500

    hdhdh

--- src/containers/chatbot.js
+++ src/containers/chatbot.js
@@ -677,58 +677,67 @@ const ChatbotComponent = () => {
         visible={isModalVisible}
         onOk={handleOk}
         onCancel={handleCancel}
-        width="80%"
+        width="50%"
         className="custom-modal-class" // Use this class to override styles
       >
-        <div className="fixed rounded-lg z-10 w-3/6 mobile:w-full h-4/6 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 border border-gray-300 shadow-md">
-          <button
-            className="absolute top-2 right-2 text-gray-500 hover:text-red-500"
-            onClick={handleCancel} // Replace `onClose` with `handleCancel` to use AntD Modal's cancel functionality
-          >
-            X
-          </button>
-          <div className="flex flex-col items-center md:items-start justify-center space-y-4 mt-12">
-            <p className="w-full md:w-2/4 text-center font-bold text-2xl ">
-              One more step...
-            </p>
-            <p className="w-full md:w-2/4 text-center text-lg">
-              We have sent a verification link to:
-            </p>
-            <p className="w-full md:w-2/4 text-center">{email}</p>
-            <p className="w-full md:w-3/4 text-center">
-              Please check your inbox (or spam inbox) and click the verification
-              link in it.
-            </p>
-          </div>
+        <div className="flex flex-col md:flex-row items-center md:items-start">
+          <div className="fixed rounded-lg z-10 w-3/6 mobile:w-full h-4/6 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 border border-gray-300 shadow-md">
+            <button
+              className="absolute top-2 right-2 text-gray-500 hover:text-red-500"
+              onClick={handleCancel}
+            ></button>
+            <div className="flex flex-col items-center md:items-start justify-center space-y-4 mt-12">
+              <p
+                className="w-full md:w-2/4 text-center font-bold text-2xl "
+                style={{ fontFamily: "Inter" }}
+              >
+                One more step...
+              </p>
+              <p className="w-full md:w-2/4 text-center text-lg">
+                We have sent a verification link to:
+              </p>
+              <p className="w-full md:w-2/4 text-center">{email}</p>
+              <p className="w-full md:w-3/4 text-center">
+                Please check your inbox (or spam inbox) and click the
+                verification link in it.
+              </p>
+            </div>
 
-          <div className="flex items-center ml-80 mobile:ml-40 mt-10">
-            <img
-              src={arrow}
-              alt="Arrow"
-              className="h-12 w-12 mobile:h-9 mobile:w-9 object-cover -rotate-45 scale-y-[-1]"
-            />
-            <img
-              src={arrow}
-              alt="Arrow"
-              className="h-12 w-12 mobile:h-9 mobile:w-9 object-cover rotate-180"
-            />
-            <img
-              src={arrow}
-              alt="Arrow"
-              className="h-12 w-12 mobile:h-9 mobile:w-9 object-cover rotate-45 scale-y-[-1] scale-x-[-1]"
-            />
-          </div>
-
-          <div className="border-t-2 border-b-2 border-black flex w-full md:mt-0 mt-5 -p-2">
-            <p className="w-full md:w-1/3 p-4">Jack Raintbolt</p>
-            <p
-              className="w-full md:w-2/3 p-4"
-              onClick={handleParagraphClick}
-              style={{ cursor: "pointer" }}
-              loading={loadings}
+            <div
+              className="flex items-center ml-80 mobile:ml-40 mt-5 "
+              style={{ marginLeft: "270px" }}
             >
-              [Action Needed] Please Verify Your Email
-            </p>
+              <img
+                src={arrow}
+                alt="Profile"
+                className="h-12 w-12 mobile:h-9 mobile:w-9  object-cover -rotate-45 scale-y-[-1]  "
+              />
+              <img
+                src={arrow}
+                alt="Profile"
+                className="h-12 w-12 mobile:h-9 mobile:w-9  object-cover rotate-180"
+              />
+              <img
+                src={arrow}
+                alt="Profile"
+                className="h-12 w-12 mobile:h-9 mobile:w-9  object-cover rotate-45 scale-y-[-1] scale-x-[-1]"
+              />
+            </div>
+
+            <div className="border-t-2 border-b-2 border-black flex w-full md:mt-0 mt-2 -p-2">
+              <p className="w-full md:w-1/3 p-1 font-semibold">
+                Jack Raintbolt
+              </p>
+
+              <p
+                className="w-full md:w-2/3 p-1"
+                onClick={handleParagraphClick}
+                style={{ cursor: "pointer" }}
+                loading={loadings}
+              >
+                [Action Needed] Please Verify Your Email
+              </p>
+            </div>
           </div>
         </div>
       </Modal>
--- src/containers/pages/profile.js
+++ src/containers/pages/profile.js
@@ -1,90 +1,83 @@
-import React,{useState,useEffect} from 'react';
-import { Button, Form, Input,notification } from 'antd';
-import './style.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Button, Form, Input, notification } from "antd";
+import "./style.css";
+import axios from "axios";
 
 const Profile = () => {
-    const [form] = Form.useForm();
-    const [userData, setUserData] = useState({});
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-              const response = await axios.get('user'); // Replace with your API endpoint to fetch user data
-              const user = response.data;
-              setUserData(user);
-              form.setFieldsValue({
-                id:user.id,
-                username: user.username,
-              });
-            } catch (error) {
-              console.error(error);
-            }
-          };
-      
-          fetchUserData();
-      }, []);
-
-    const openNotificationWithIconRole = (type, description) => {
-        notification.destroy();
-        notification[type]({
-          message: type === 'success' ? 'Operation Successful' : 'Operation Failed',
-          description: description,
+  const [form] = Form.useForm();
+  const [userData, setUserData] = useState({});
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get("user"); // Replace with your API endpoint to fetch user data
+        const user = response.data;
+        setUserData(user);
+        form.setFieldsValue({
+          id: user.id,
+          username: user.username,
         });
-      };
-    const onFinish = async (values) => {
-        try {
-            // Make an Axios request to update the username and password
-            await axios.post('updateUser', values);
-      
-            openNotificationWithIconRole('success', 'Password changed successfully');
-          } catch (error) {
-            console.error(error);
-            openNotificationWithIconRole('error', 'Failed to change password');
-          }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-      };
-      const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-      };
+    fetchUserData();
+  }, []);
 
-    return(
-        <Form
-        name="profile"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-        form={form}
-        style={{color:'black'}}
+  const openNotificationWithIconRole = (type, description) => {
+    notification.destroy();
+    notification[type]({
+      message: type === "success" ? "Operation Successful" : "Operation Failed",
+      description: description,
+    });
+  };
+  const onFinish = async (values) => {
+    try {
+      // Make an Axios request to update the username and password
+      await axios.post("updateUser", values);
+
+      openNotificationWithIconRole("success", "Password changed successfully");
+    } catch (error) {
+      console.error(error);
+      openNotificationWithIconRole("error", "Failed to change password");
+    }
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      name="profile"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      form={form}
+      style={{ color: "black" }}
+    >
+      <Form.Item label="Username" name="username">
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Update Password" name="password">
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
       >
-
-        <Form.Item
-          label="Username"
-          name="username"
+        <Button
+          type="button"
+          className="bg-blue-500 text-white"
+          htmlType="submit"
         >
-          <Input />
-        </Form.Item>
-    
-        <Form.Item
-          label="Update Password"
-          name="password"
-        >
-          <Input />
-        </Form.Item>
-    
-        
-    
-        <Form.Item
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
-          <Button type="button" className='bg-blue-500 text-white' htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    )
-
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 export default Profile;
