2f8832876b4491c09f29bd4b868a63eca987adbc
Zafar Ahmed <zafar.ahmed2k17@gmail.com>
2023-12-26 20:14:07 +0500

    hdhhd

---
+++ src/containers/botChat.js
@@ -0,0 +1,42 @@
+export const messageArray1 = [
+  {
+    type: "bot",
+    content: "Before we get started, what is your Name and Email address",
+  },
+  { type: "email&phone", content: "" },
+];
+export const messageArray = [
+  {
+    type: "bot",
+    content: "Acknowledged! Lets get started with 3 Quick Questions",
+  },
+  { type: "bot", content: "1. Are you familiar with cryptocurrency trading?" },
+  { type: "YesNo", content: "" },
+];
+export const messageArray2 = [
+  {
+    type: "bot",
+    content:
+      "2. How much money would you estimate you have invested in cryptocurrencies?",
+  },
+  { type: "currency", content: "" },
+];
+export const messageArray3 = [
+  {
+    type: "bot",
+    content: "3. When do you intend to start profiting?",
+  },
+  { type: "date", content: "" },
+];
+
+export const messageArray4 = [
+  {
+    type: "bot",
+    content:
+      "How do we show, share, and send the profit from your copied traders back to you.",
+  },
+  { type: "card", content: "Mobile Number" },
+  { type: "card", content: "WhatsApp" },
+  { type: "card", content: "Telegram" },
+  { type: "telegram&whatsapp", content: "" },
+];
